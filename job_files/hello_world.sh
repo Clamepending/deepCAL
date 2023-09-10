c01966a64a9593852a108183484569d0a442c8fd
Mark Ogata <ogata@ln001.brc>
2023-09-09 22:09:40 -0700

    reformated helloworld

--- job_files/hello_world.sh
+++ job_files/hello_world.sh
@@ -1,15 +1,15 @@
-    #!/bin/bash
-    # Job name:
-    #SBATCH --job-name=hello_world
-    #
-    # Account:
-    #SBATCH --account=fc_contact
-    #
-    # Partition:
-    #SBATCH --partition=savio3
-    #
-    # Wall clock limit:
-    #SBATCH --time=00:00:30
-    #
-    ## Command(s) to run:
-    echo "hello world"
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=hello_world
+#
+# Account:
+#SBATCH --account=fc_contact
+#
+# Partition:
+#SBATCH --partition=savio3
+#
+# Wall clock limit:
+#SBATCH --time=00:00:30
+#
+## Command(s) to run:
+echo "hello world"
